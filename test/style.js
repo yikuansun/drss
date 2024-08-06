9620937ec27ebd6a63f6e3f3bd1ebc09977165ca
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-06 10:24:03 -0400

    Move demo style to a separate file

---
+++ test/style.js
@@ -0,0 +1,62 @@
+// yep... you've probably never seen a style.js file before.
+// trust me, though - after a while using RSS, you'll never want to make a style.css file again.
+
+// Apply styles & set up auto-update.
+RSS.initialize();
+
+RSS.setProps({
+    evenBG: "black",
+    oddBG: "grey",
+});
+
+// static ruleset
+RSS.select("h1").ruleset({
+    fontFamily: "cursive", // "font-family" also works.
+});
+
+// dynamic ruleset
+RSS.select("h1").ruleset((node, index, props) => {
+    return {
+        color: node.dataset.color || "white", // use data-color of element, or white.
+        backgroundColor: (index % 2 == 0)?(props["evenBG"]):(props["oddBG"]), // alternate colors
+    };
+});
+
+// hover state
+RSS.select("h1", "hover").ruleset((node, index, props) => {
+    return {
+        color: "green",
+    };
+});
+RSS.select("button", "hover").ruleset({
+    color: "blue",
+});
+
+// focus state
+RSS.select("button", "focus").ruleset((node, index, props) => {
+    return {
+        color: "green",
+    };
+});
+
+// you can stylize multiple states simultaneously
+RSS.select("li", ["before", "after"]).ruleset({
+    content: "'--'", // need additional quotes around CSS string
+    color: "blue",
+});
+
+// scrollbars!
+RSS.select("body").ruleset({
+    overflow: "scroll"
+});
+RSS.select("body", ":-webkit-scrollbar").ruleset({ // add an extra : since it's a pseudo
+    width: "10px",
+    height: "10px",
+    backgroundColor: "aliceblue",
+});
+RSS.select("body", ":-webkit-scrollbar-thumb").ruleset({
+    backgroundColor: "grey",
+});
+RSS.select("body", ":-webkit-scrollbar-thumb:hover").ruleset({
+    backgroundColor: "darkblue",
+});
